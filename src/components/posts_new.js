987e6e161c2a74f2e813f9c6f171fdec2b62e5aa
Yondy Kang <yokangy@gmail.com>
2016-02-21 12:28:12 -0800

    New blog posts can be created and saved to server.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+    const { fields: { title, categories, content }, handleSubmit } = this.props;
+    // const handleSubmit = this.props.handleSubmit;
+    
     return (
-      <div>Create Form</div>
+      <form onSubmit={handleSubmit(this.props.createPost)}>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-control" {...title} />
+        </div>
+
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-control" {...categories} />
+        </div>
+
+        <div className="form-group">
+          <label>Content</label>
+          <textarea type="text" className="form-control" {...content} />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
+// connect first arg is mapStateToProp, 2nd mapDispatchToProps
+// reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd mapDispatchToProps
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+}, null, { createPost })(PostsNew);
