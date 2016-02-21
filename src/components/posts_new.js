e85d64ba67bf4fd6bb650537b2af867c9d5fbda6
Yondy Kang <yokangy@gmail.com>
2016-02-21 13:40:33 -0800

    Added validations to the create new post form.

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -10,19 +10,28 @@ class PostsNew extends Component {
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
-        <div className="form-group">
+        <div className={`form-group ${title.touched && title.invalid ? 'has-danger' :''}`}>
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
+          <div className="text-help">
+            {title.touched ? title.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${categories.touched && categories.invalid ? 'has-danger' :''}`}>
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {categories.touched ? categories.error : ''}
+          </div>
         </div>
 
-        <div className="form-group">
+        <div className={`form-group ${content.touched && content.invalid ? 'has-danger' :''}`}>
           <label>Content</label>
           <textarea type="text" className="form-control" {...content} />
+          <div className="text-help">
+            {content.touched ? content.error : ''}
+          </div>
         </div>
 
         <button type="submit" className="btn btn-primary">Submit</button>
@@ -31,10 +40,29 @@ class PostsNew extends Component {
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  if(!values.title) {
+    errors.title = 'Enter a username';
+  }
+
+  if(!values.categories) {
+    errors.categories = 'Enter categories';
+  }
+
+  if(!values.content) {
+    errors.content = 'Enter content';
+  }
+
+  return errors;
+}
+
 // connect first arg is mapStateToProp, 2nd mapDispatchToProps
 // reduxForm: 1st is form config, 2nd is mapStateToProps, 3rd mapDispatchToProps
 
 export default reduxForm({
   form: 'PostsNewForm',
-  fields: ['title', 'categories', 'content']
+  fields: ['title', 'categories', 'content'],
+  validate
 }, null, { createPost })(PostsNew);
