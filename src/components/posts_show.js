691aaf7dcf628ecbc2e9f94de92e63d95f0d39b4
Yondy Kang <yokangy@gmail.com>
2016-02-21 23:43:16 -0800

    Allow the user to delete the blog post from the post details page and navigate back to the index page after deleting.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,12 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions/index';
+import { fetchPost, deletePost } from '../actions/index';
 import { Link } from 'react-router';
 
 class PostsShow extends Component {
+  static contextTypes = {
+    router: PropTypes.object
+  };
+
   componentWillMount() {
     this.props.fetchPost(this.props.params.id);
   }
+
+  onDeleteClick() {
+    this.props.deletePost(this.props.params.id)
+      .then(() => {
+        this.context.router.push("/");
+      });
+  }
+
   render() {
     const { post } = this.props;
     //const post = this.props.post;
@@ -21,6 +33,11 @@ class PostsShow extends Component {
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
         <Link to='/'>Back to index</Link>
+        <button 
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}>
+          Delete Post
+        </button>
       </div>
     );
   }
@@ -30,4 +47,4 @@ function mapStateToProps(state) {
   return { post: state.posts.post };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
