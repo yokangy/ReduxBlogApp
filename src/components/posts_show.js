e458c828431dc5f9967c29670356e04ab5050aae
Yondy Kang <yokangy@gmail.com>
2016-02-21 20:58:22 -0800

    Added a link on the blog post detail page to return to the blog index page.

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchPost } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsShow extends Component {
   componentWillMount() {
@@ -19,6 +20,7 @@ class PostsShow extends Component {
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
+        <Link to='/'>Back to index</Link>
       </div>
     );
   }
